ce95556cffee9396923a52b46d72931b56147494
pcadmin <zuchii.victor@gmail.com>
2023-10-16 14:41:27 -0300

    criando o dicionario

---
+++ projeto/amanda/dicio.py
@@ -0,0 +1,52 @@
+dicionario_principal = {
+    "A culpa é das estrelas": {
+        "autor": "John Green",
+        "genero": "Romance",
+        "ano_publicacao": "2012"
+    },
+    "Harry Potter e a Pedra Filosofal": {
+        "autor": "J. K. Rowling",
+        "genero": "Fantasia",
+        "ano_publicacao": "1997"
+    },
+    "It - A Coisa": {
+        "autor": "Stephen King",
+        "genero": "Terror",
+        "ano_publicacao": "1996"
+    },
+    "Jogos Vorazes": {
+        "autor": "Suzanne Collins",
+        "genero": "Distopia",
+        "ano_publicacao": "2008"
+    },
+    "A Garota no Trem": {
+        "autor": "Paula Hawkins",
+        "genero": "Mistério",
+        "ano_publicacao": "2015"
+    },
+    "Livro 6": {
+        "autor": "Nome_Autor 6",
+        "genero": "Aventura",
+        "ano_publicacao": "30112010"
+    },
+    "Livro 7": {
+        "autor": "Nome_Autor 7",
+        "genero": "Romance",
+        "ano_publicacao": "22082017"
+    },
+    "Livro 8": {
+        "autor": "Nome_Autor 8",
+        "genero": "Ficção Científica",
+        "ano_publicacao": "12042003"
+    },
+    "Livro 9": {
+        "autor": "Nome_Autor 9",
+        "genero": "Fantasia",
+        "ano_publicacao": "18062015"
+    },
+    "Livro 10": {
+        "autor": "Nome_Autor 10",
+        "genero": "Mistério",
+        "ano_publicacao": "03032009"
+    }
+}
