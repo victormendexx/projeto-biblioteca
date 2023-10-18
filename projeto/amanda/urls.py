2fa4ae4f638b130043e9951cf613f664eaa66c0b
pcadmin <isabela.bnu@gmail.com>
2023-10-18 13:49:05 -0300

    ok

--- projeto/amanda/urls.py
+++ projeto/amanda/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from amanda.views import inicio, avaliacoes, sobre, mostrar_livros
-from amanda.views import LoginView, LoginViewClass, LogoutViewClass
+from amanda.views import LoginViewClass, LogoutViewClass
 
 urlpatterns = [
     path('login/', LoginViewClass.as_view(), name='login'),
