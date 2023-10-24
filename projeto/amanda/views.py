2b671558165d86e02c77fa6dda230ea1be9e635e
Victor Mendes <victorgomesmendes05@gmail.com>
2023-10-24 11:38:56 -0300

    deletando tudo e indo ver como esta o da ana

--- projeto/amanda/views.py
+++ projeto/amanda/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from .forms import CommentForm
 from django.contrib.auth.views import LoginView, LogoutView
 from .dicio import dicionario_principal, status_emprestimo
-from django.http import FileResponse
-from django.views.decorators.csrf import csrf_exempt
-from django.urls import reverse
-import os
 # from .models import UserProfile 
 
 class LoginViewClass(LoginView):
@@ -58,29 +54,3 @@ def detalhes_livros(request, livro_id):
         return HttpResponse("Livro não encontrado")
 
     return render(request, 'amanda/detalhes_livros.html', {'livro': livro, 'status_emprestimo': status, 'livro_id': livro_id})
-
-
-def pegar_emprestado(request, livro_id):
-    status_info = status_emprestimo.get(livro_id)
-    
-    if status_info and status_info["status"] == "Disponível":
-        pdf_path = status_info.get("pdf_disponivel", "")
-        
-        if pdf_path:
-            with open(pdf_path, 'rb') as pdf_file:
-                response = FileResponse(pdf_file, content_type='application/pdf')
-                response['Content-Disposition'] = f'attachment; filename="{os.path.basename(pdf_path)}'
-                status_info['status'] = "Indisponível"
-                return response
-    return HttpResponse("Livro não encontrado ou indisponível")
-
-def devolver_emprestado(request, livro_id):
-    if request.method == 'POST':
-        uploaded_file = request.FILES['pdf_upload']
-        status_info = status_emprestimo.get(livro_id)
-
-        if status_info['status'] and uploaded_file.name == os.path.basename(status_info.get("pdf_disponivel", "")):
-            status_info['status'] = "Disponível"
-            return HttpResponse("Livro devolvido com sucesso")
-    
-    return HttpResponseRedirect(reverse('detalhes_livros', kwargs={'livro_id': livro_id}))
