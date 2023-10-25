180101bca469b814ccfdf61040bbb7e22e267c87
Arthur H. Erhardt <arthurerhardt1979@gmail.com>
2023-10-25 14:01:00 -0300

    daily25/10

--- projeto/amanda/urls.py
+++ projeto/amanda/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
 
     path('', inicio, name='inicio'),
-    path('sobre', sobre, name='sobre'),
-    path('catalogo', catalogo, name = 'catalogo'),
+    path('sobre/', sobre, name='sobre'),
+    path('catalogo/', catalogo, name = 'catalogo'),
     path('login/', LoginViewClass.as_view(), name='login'),
     path('logout/', LogoutViewClass.as_view(), name='logout'),
     path('livro<int:livro_id>/', detalhes_livros, name='detalhes_livros'),
