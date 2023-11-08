44a53f0b7ad26530554c205807a70bd3d3f1b95f
Ivanir Stano <ryanwds101@gmail.com>
2023-11-08 13:48:15 -0300

    sqlite

--- projeto/amanda/admin.py
+++ projeto/amanda/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Livro, Avaliacao
 
-# Register your models here.
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'autor', 'genero', 'editora', 'ano_publicacao', 'status')
+    search_fields = ('titulo', 'autor', 'genero', 'editora')
+    list_filter = ('status', 'genero')
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('livro', 'nota', 'comentario')
+    list_filter = ('nota',)
--- projeto/amanda/models.py
+++ projeto/amanda/models.py
@@ -1,10 +1,25 @@
-#from django.contrib.auth.models import User
 from django.db import models
 
-#class Comment(models.Model):
-#    name = models.CharField(max_length=100)
-#    text = models.TextField()
-#    timestamp = models.DateTimeField(auto_now_add=True)
+class Livro(models.Model):
+    id = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    autor = models.CharField(max_length=100)
+    genero = models.CharField(max_length=50)
+    editora = models.CharField(max_length=100)
+    ano_publicacao = models.IntegerField()
+    idioma = models.CharField(max_length=50)
+    imagem = models.ImageField(upload_to='livros/')
+    sinopse = models.TextField()
+    status = models.CharField(max_length=20, default="Disponível")
+    pdf_disponivel = models.FileField(upload_to='pdfs/')
+    def __str__(self):
+        return self.titulo
 
-#    def __str__(self):
-#        return self.name
+class Avaliacao(models.Model):
+    id = models.AutoField(primary_key=True)
+    livro = models.ForeignKey(Livro, on_delete=models.CASCADE)
+    nota = models.IntegerField()
+    comentario = models.TextField()
+
+    def __str__(self):
+        return f"Avaliação de {self.livro.titulo} - Nota: {self.nota}"
