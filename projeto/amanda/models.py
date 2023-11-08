2c7a112d20d21936ba3750b923283bd96eecb338
Ivanir Stano <ryanwds101@gmail.com>
2023-11-08 13:36:38 -0300

    teste

--- projeto/amanda/models.py
+++ projeto/amanda/models.py
@@ -1,19 +1,10 @@
 #from django.contrib.auth.models import User
 from django.db import models
 
-class Comment(models.Model):
-    name = models.CharField(max_length=100)
-    text = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+#class Comment(models.Model):
+#    name = models.CharField(max_length=100)
+#    text = models.TextField()
+#    timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.name
-    
-    
-# class UserProfile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
-#     # Adicione outros campos personalizados
-
-#     def __str__(self):
-#         return self.user.username
+#    def __str__(self):
+#        return self.name
